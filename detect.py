584e1f018f9afad9a43d45ed8113662cab01771a
Aneesh C <aneesh.c@cognub.com>
2018-09-19 21:23:44 +0530

    code change

--- detect.py
+++ detect.py
@@ -1,10 +1,39 @@
 from imageai.Detection import ObjectDetection
 import os
-
+import sys
+import numpy as np
 import cv2
+from sklearn.cluster import KMeans
+import matplotlib.pylab as plt
+import matplotlib.patches as patches
+import kmedoids
+ROOT_DIR = os.path.abspath("../")
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+import mrcnn.model as modellib
+import coco
 
-file_name = '19UDE2F32HA002792_1.jpg'
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+file_name = 'c_h_1.jpg'
 execution_path = os.getcwd()
+
+
+lst_intensities = []
+
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
@@ -13,12 +42,21 @@ detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, file_name),
                                              output_image_path=os.path.join(execution_path, "imagenew.jpg"))
 
+img = cv2.imread(os.path.join(execution_path, file_name))
+
+
+
 temp = 0
-for eachObject in detections:
-    if eachObject['name'] == 'car':
-        if eachObject['percentage_probability'] > temp:
-            temp = eachObject['percentage_probability']
-            array = eachObject['box_points']
+
+if len(detections) !=0:
+    for eachObject in detections:
+        if eachObject['name'] == 'car' or eachObject['name'] == 'truck':
+            area = (eachObject['box_points'][2]- eachObject['box_points'][0])+ (eachObject['box_points'][3]- eachObject['box_points'][1])
+            if area>temp:
+                temp = area
+                array = eachObject['box_points']
+else:
+    array = []
 
         # print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
 
@@ -27,53 +65,97 @@ def preprocess(rgb):
     blurred = cv2.GaussianBlur(rgb, (5, 5), 0)
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
     #   print ('gray_shape', gray.shape)
-    binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
-    thresh = binary.max() - binary
-    return thresh
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                          cv2.THRESH_BINARY, 11, 2)
+
+    return binary
 
 
 def find_countours(binary_img):
     _, cnts, _ = cv2.findContours(binary_img.copy(), cv2.RETR_EXTERNAL,
                                   cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    cnts = [cnts[0]]
     return cnts
 
 
-x1, y1, x2, y2 = array[0], array[1], array[2], array[3]
+def extract_patch_mask(img, array):
+    x1, y1, x2, y2 = array[0], array[1], array[2], array[3]
+    sub_img = img[y1:y2, x1:x2]
+    mask = np.zeros_like(sub_img)
+    return sub_img, mask
 
-img = cv2.imread(os.path.join(execution_path, file_name))
 
-#cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+def draw_countours(cnts, sub_img, mask):
+    lst_intensities = []
 
-sub_img = img[y1:y2, x1:x2]
+    for c in cnts:
 
-# print ('shape', sub_img.shape)
-binary = preprocess(sub_img)
+        c = c.astype("float")
+        c *= 1
+        c = c.astype("int")
 
-cv2.imshow('"', binary)
-cnts = find_countours(binary)
-print (len(cnts))
-#print ('cns', cnts)
-for c in cnts:
-    # compu1te the center of the contour
-    M = cv2.moments(c)
-    cX = int((M["m10"] / M["m00"]))
-    cY = int((M["m01"] / M["m00"]))
 
-    # detect the shape of the contour and label the color
+        cv2.drawContours(mask, [c], -1, (255, 255, 255), -1)
+        pts = np.where(mask == 255)
+        mask[mask == 255] = 1
+        lst_intensities.append(sub_img[pts[0], pts[1]])
+    return lst_intensities, mask
 
-    #	color = cl.label(lab, c)
 
-    # multiply the contour (x, y)-coordinates by the resize ratio,
-    # then draw the contours and the name of the shape and labeled
-    # color on the image
-    c = c.astype("float")
-    c *= 1
-    c = c.astype("int")
-    #	text = "{}".format(color)
-    cv2.drawContours(sub_img, [c], -1, (255, 0, 0), 2)
-    #	cv2.putText(image, text, (cX, cY),
-    #		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+def run_kmeans(lst_intensities):
+    array = np.array(lst_intensities[0])
+    kmeans = KMeans(n_clusters=3, max_iter=10000)
+    kmeans.fit(array)
 
+    unique_l, counts_l = np.unique(kmeans.labels_, return_counts=True)
+
+    sort_ix = np.argsort(counts_l)
+    sort_ix = sort_ix[::-1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x_from = 0.05
+
+    for cluster_center in kmeans.cluster_centers_[sort_ix]:
+        print('point', int(cluster_center[2]), int(cluster_center[1]), int(cluster_center[0]))
+        ax.add_patch(patches.Rectangle((x_from, 0.05), 0.29, 0.9, alpha=None,
+                                       facecolor='#%02x%02x%02x' % (
+                                       int(cluster_center[2]), int(cluster_center[1]), int(cluster_center[0]))))
+        x_from = x_from + 0.31
+
+    plt.show()
+
+
+if len(array) !=0:
+    sub_img, mask = extract_patch_mask(img, array)
+    cv2.imwrite('test.jpg', sub_img)
+    binary = preprocess(sub_img)
+    cnts = find_countours(binary)
+    lst_intensities, mask = draw_countours(cnts, sub_img, mask)
     cv2.imshow('', sub_img)
     cv2.waitKey(0)
+    mul = mask * sub_img
+    cv2.imshow('', mul)
+    cv2.waitKey(0)
+    run_kmeans(lst_intensities)
+else:
+    print ('No car found')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
